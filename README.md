55bc8d3936c3dc12a8ff8f687ea343ed434f0b42
Zac McCormick <zac.mccormick@gmail.com>
2011-12-06 22:12:42 -0500

    foxsoccer.tv, yo

---
+++ README.md
@@ -0,0 +1,8 @@
+# foxsoccer.tv minify script for Firefox + Greasemonkey
+This is a little script to minify the foxsoccer.tv UI. Add it to Greasemonkey with the url http://www.foxsoccer.tv/watchdvr/*
+
+It hides all of the UI and links on the Flash player page, so getting back to the listing is manual (for now).
+
+For a super minimal experience, put the page in its own window and hide all of the Firefox toolbars from View -> Toolbars.
+
+Enjoy
---
+++ foxsoccer.tv.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name           minimalize foxsoccer.tv
+// @namespace      mcc
+// @description    minimalize foxsoccer.tv
+// @include        http://www.foxsoccer.tv/watchdvr/*
+// ==/UserScript==
+
+function minimalize() {
+	document.body.style.background = '#fff';
+	document.body.style.backgroundImage = 'none';
+	document.getElementsByClassName('content')[0].style.background = '#fff';
+	document.getElementsByClassName('content')[0].style.backgroundImage = 'none';
+	document.getElementsByClassName('content')[0].style.padding = '0px';
+	document.body.getElementsByTagName('header')[0].style.display = 'none';
+	document.body.getElementsByTagName('footer')[0].style.display = 'none';
+	document.getElementsByClassName('mainContent')[0].style.margin = '0px';
+	document.getElementsByClassName('mainContent')[0].style.padding = '0px';
+	document.getElementsByClassName('mainContent')[0].style.width = '958px';
+	document.getElementsByClassName('mainContent')[0].style.height = '538px';
+	document.getElementsByClassName('mainContent')[0].style.overflow = 'hidden';
+	document.getElementsByClassName('ondemand')[0].style.display = 'none';
+	document.getElementsByClassName('fs-footer')[0].style.display = 'none';
+	document.getElementsByClassName('twitter')[0].style.display = 'none';
+	document.body.style.overflow = 'hidden';
+		
+	try {
+		document.getElementsByTagName('form')[0].style.display = 'none';
+		document.getElementsByTagName('form')[1].style.display = 'none';
+		document.getElementsByTagName('form')[2].style.display = 'none';
+	} catch (ex) {
+	}
+	
+	window.innerWidth = 956;
+	window.innerHeight = 538;
+}
+
+window.addEventListener('load', minimalize, false);
+
+
